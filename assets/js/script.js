60d8b8926661c32842c28bf3679fe492454333e2
ZeuOne <maximewalpoel@gmail.com>
2025-02-13 20:29:18 +0100

    .

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let mainSite = document.querySelector("#mainSite");
+
+function lunchIntro() {
+    setTimeout(() => {
+      mainSite.style.display = "block"; 
+    }, 3000); 
+  }
+
+  lunchIntro();
